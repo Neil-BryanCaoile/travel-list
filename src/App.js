0ccffcd8ea5fb662ab8d949ec5f11e38b3e2de90
Neil Bryan Caoile <77993480+Neil-BryanCaoile@users.noreply.github.com>
2023-09-29 11:28:19 -0400

    Removed Initial Items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Socks", quantity: 12, packed: false },
-];
-
 // The main App, that will be use in index.js
 export default function App() {
   const [items, setItems] = useState([]);
